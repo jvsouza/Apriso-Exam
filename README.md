059b3634619615e686c9bfd0df29673cc021490f
Jonas Vieira de Souza <jonas.vieira.de.souza@gmail.com>
2022-08-24 07:53:45 -0300

    Initial commit
    up

---
+++ README.md
@@ -0,0 +1,58 @@
+# Apriso-Exam
+![Progress](https://progress-bar.dev/1/?title=Completed%20&width=160&color=54aeff)
+
+## Collection of test prep study questions for 3DS certification - Delmia Apriso.
+> [As specified by 3DS](https://www.3ds.com/products-services/delmia/products/delmia-apriso/), the Delmia Apriso product features a set of certificates that can be obtained through examination.
+
+## For each exam studied on the 3DExperience portal, I store the relevant questions in `.json` files. Below the list of `.json` files:
+| Type | Index | Progress | Contet |
+| :--: | :---: | :------: | :----- |
+| Apriso | 01 | ![](https://img.shields.io/badge/-Done-success) | BPM Use - Level 1 |
+| Apriso | 02 | ![](https://img.shields.io/badge/-Done-success) | Brand Essentials |
+
+## For that, I'm storing studied questions in this collection. As webpage struct bellow:
+```text
+page
+├── css
+│   ├── bootstrap.min.css
+│   ├── bootstrap.min.css.map
+│   └── custom.css
+├── img
+│   ├── flag_br.png
+│   └── flag_en.png
+├── js
+│   ├── aes.py
+│   ├── app.py
+│   ├── bootstrap.min.js
+│   ├── bootstrap.min.js.map
+│   └── jquery.min.py
+├── json
+│   ├── apriso01.json
+│   └── apriso02.json
+├── encrypt.html
+├── favicon.png
+├── index.html
+├── LICENSE
+└── README.md
+
+```
+
+> Link of Github Page created: https://jvsouza.github.io/Apriso-Exam/
+
+## Shields
+[![](https://img.shields.io/github/languages/top/jvsouza/Apriso-Exam)]()
+[![](https://img.shields.io/github/languages/count/jvsouza/Apriso-Exam)]()
+[![](https://img.shields.io/github/license/jvsouza/Apriso-Exam)]()
+
+[![](https://img.shields.io/github/languages/code-size/jvsouza/Apriso-Exam)]()
+[![](https://img.shields.io/github/repo-size/jvsouza/Apriso-Exam)]()
+[![](https://img.shields.io/github/last-commit/jvsouza/Apriso-Exam)]()
+
+## Badges
+![HTML](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
+![jQuery](https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)
---
+++ js/app.js
@@ -0,0 +1,157 @@
+
+// ...
+var questions_available = [];
+const letter_up = ["A", "B", "C", "D"];
+//const baseUrl = 'https://raw.githu  busercontent.com/jvsouza/ANAC-Cell/main/json/';
+const baseUrl = 'http://localhost/Apriso-Quiz/json/';
+const coursesUpdated = {
+    "courses_updated":[
+        {"file_json":"apriso01", "course_unit":"01 - BPM Use - Level 1"},
+        {"file_json":"apriso02", "course_unit":"02 - Brand Essentials"},
+    ]
+};
+
+// ...
+function decryptText(encryptedText){
+    let keyView = $("#encry_key").val();
+    let decryptedText = CryptoJS.AES.decrypt(encryptedText, keyView);
+    return String(decryptedText.toString(CryptoJS.enc.Utf8));
+}
+
+// ...
+function createList() {
+    let questions = questions_available.questions;
+    if ( questions != null ){
+        var list = '<ul class="ulTop list-group">';
+        var letter = '';
+        for (var n=0; n < questions.length ; n++) {
+            list += '<li class="liTop list-group-item" data-li="'+n+'">';   
+                list += '<ul class="ulMid list-group">';
+                    list += '<li class="liMed title list-group-item">';
+                        list += '<div class="row">';
+                            list += '<div class="col-1 text-center align-items-center justify-content-center d-inline-flex"><label>';
+                                list += parseInt(n+1, 10);
+                            list += '</label></div>';
+                            list += '<div class="col-9">';
+                                    list += '<div class="titles_itens">';
+                                        list += '<div class="titles_item">';
+                                            list += '<div class="bandeira"><img src="img/flag_en.png" /></div>';
+                                            list += '<div class="title_text">';
+                                                list += questions[n].title["en"];
+                                            list += '</div>';
+                                        list += '</div>';
+                                        list += '<div class="titles_item">';
+                                            list += '<div class="bandeira"><img src="img/flag_br.png" /></div>';
+                                            list += '<div class="title_text">';                                        
+                                                list += questions[n].title["br"];
+                                            list += '</div>';
+                                        list += '</div>';
+                                    list += '</div>';
+                            list += '</div>';
+                            list += '<div class="col-2">';
+                                list += '<button type="button" class="btn btn-outline-primary w-100 h-100" data-question="'+ n +'"></button>';
+                            list += '</div>';
+                        list += '</div>';
+                    list += '<li class="liMed options">';
+                        list += '<div class="list-group" data-select="'+n+'">';
+                            for (var i = 0; i < questions[n].options["br"].length; i++) {
+                                list += '<button type="button" class="list-group-item list-group-item-action button_itens" value="'+ i +'">';
+                                    list += '<div class="letter_up">' + letter_up[i] + '</div>';
+                                    list += '<div class="questions_itens">';
+                                        //list += '<div class="question_item"><div class="bandeira"><img src="img/flag_en.png" /></div><div class="question_text">' + decryptText(questions[n].options["en"][i]) + '</div></div>';
+                                        //list += '<div class="question_item"><div class="bandeira"><img src="img/flag_br.png" /></div><div class="question_text">' + decryptText(questions[n].options["br"][i]) + '</div></div>';
+                                        list += '<div class="question_item"><div class="bandeira"><img src="img/flag_en.png" /></div><div class="question_text">' + questions[n].options["en"][i] + '</div></div>';
+                                        list += '<div class="question_item"><div class="bandeira"><img src="img/flag_br.png" /></div><div class="question_text">' + questions[n].options["br"][i] + '</div></div>';                                        
+                                    list += '</div>';
+                                list += '</button>';
+                            }
+                        list += '</div>';
+                    list += '</li>';
+                list += '</ul>';
+            list += '</li>';
+        }
+        list += '</ul>';
+        $("#questions").append(list);
+    }
+}
+
+// ...
+function getJson( nameFileJson ) {
+    fetch(nameFileJson)
+    .then(question_file => {
+        return question_file.json(); 
+    })
+    .then( questions_loaded => {
+        questions_available = questions_loaded;
+        createList();
+    })
+    .catch( err => {
+        console.log(err);
+    })
+}
+
+// ...
+function createSelect( baseUrl, cu ){
+    var options = '<option value="title" selected>Select content</option>';
+    for (var n=0; n < cu.courses_updated.length ; n++) {
+        jsonUrl = baseUrl + cu.courses_updated[n].file_json + '.json'
+        courseUnit = cu.courses_updated[n].course_unit;
+        options += '<option value="' + jsonUrl + '">' + courseUnit + '</option>';
+    }
+     $("#select").append(options);
+}
+
+// ...
+function checkKeyView(){
+    let keyView = $("#encry_key").val();
+    let textTest = "U2FsdGVkX19cv8qTrbFUdx8+8tRyanlSr/1+QWh9A2A=";
+    let decryptedText = CryptoJS.AES.decrypt(textTest, keyView);
+    let decryptedString = decryptedText.toString(CryptoJS.enc.Utf8);
+
+    if (decryptedString == "ok") {
+        return true
+    } else {
+        return false
+    }
+}
+
+// ...
+$(document).ready(function(){
+    // ...
+    $(document).on('click','.btn', function() {
+        let questionNumber = $(this).data('question');
+        let correctOption = questions_available.questions[questionNumber].result;
+        for (var i = 0; i < correctOption.length; i++) {
+            $("li[data-li="+questionNumber+"] li.options div button[value="+correctOption[i]+"]").addClass( "optionCorrect" );    
+        }
+    });
+
+    // ...
+    $(document).on("change","#select",function(){
+        //if (checkKeyView() == true ) {
+        if ( true == true ) {
+            let nameJson = $(this).val();
+            $("#questions").empty();
+            if (nameJson != 'title'){
+                getJson(nameJson);
+            }
+        } else {
+            let alertMessage = '<div class="alert alert-danger  alert-dismissible fade show text-center" role="alert">Key view <strong>incorrect</strong> !<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+            $("#mensagem").html(alertMessage);
+            $("#questions").empty();
+        }
+    });
+
+    // ...
+    createSelect( baseUrl, coursesUpdated );
+
+    // ...
+    $("#encry_action").click(function(){
+        let key = $("#encry_key").val();
+        let text_encrypt_not = $("#encry_not").val();
+        let text_encrypt_yes = CryptoJS.AES.encrypt(text_encrypt_not, key);
+        $("#encry_yes").val(text_encrypt_yes);
+        $("#dencrpyted").val(decryptText(text_encrypt_yes));
+
+    });
+});
